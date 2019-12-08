c91535d417af21487b06fc4a1051cfadf5dce369
krishnasan <52101501+krishnasan@users.noreply.github.com>
2019-12-08 19:09:29 +0100

    First commit

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Kanishkaa!!! Kanniga !!!"}
+		res := &response{Message: "Hello Kanishkaa && Kanniga !!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
